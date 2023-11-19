722917a5c541021aa9655a57513d476a59109a7a
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-11-19 19:09:06 -0300

    html quase pronto - inserir label dentro do input

--- questao-3/Questao-03.php
+++ questao-3/Questao-03.php
@@ -15,36 +15,36 @@
     <div id="sections" class="container-fluid">
         <div id="first-section" class="col-sm-6">
             <div id="client-name">
-                <h3>Nome:</h3>
+                <h3 class="subtitle-construsite">Nome:</h3>
                 <h4>Gabriel Henrique</h4>
             </div>
         </div>
         <div id="second-section" class="col-sm-6">
             <form class="form-horizontal" action="index.php#formulario" method="post" role="form" data-toggle="validator">
-                <h3>Mensagem</h3>
+                <h3 class="text-left subtitle-construsite">Mensagem</h3>
                 <div class="form-group">
-                    <label class="control-label col-sm-3">Nome*:</label>
+                    <label class="col-sm-3">Nome*:</label>
                     <div class="col-sm-9">
                         <input type="text" class="form-control" name="nome" id="nome" value="" placeholder="seu nome" required >
                         <div class="help-block with-errors"></div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-3">Telefone*:</label>
+                    <label class="col-sm-3">Telefone*:</label>
                     <div class="col-sm-9">
                         <input type="number" class="form-control" name="telefone" id="telefone" placeholder="(00) 00000-0000" required>
                         <div class="help-block with-errors"></div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-3">Email*:</label>
+                    <label class="col-sm-3">Email*:</label>
                     <div class="col-sm-9">
                         <input type="email" class="form-control" name="email" id="email" value="" placeholder="exemplo@dominio.com" required>
                         <div class="help-block with-errors"></div>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-3">Mensagem*:</label>
+                    <label class="col-sm-3">Mensagem*:</label>
                     <div class="col-sm-9">
                         <textarea class="form-control" id="exampleTextarea" rows="6" 
                                     id="mensagem" name="mensagem" placeholder="sua mensagem" required></textarea>
@@ -52,8 +52,8 @@
                     </div>
                 </div>
                 <div class="form-group">
-                    <div class="col-md-12 text-right">
-                        <input class = "btn btn-primary" id="submit" name="btnSend" type="submit" value="ENVIAR">
+                    <div class="col-sm-12">
+                        <input class = "btn btn-success btn-block" id="submit" name="btnSend" type="submit" value="Enviar Mensagem">
                         <a name="formulario"></a>
                         <div class="mensagem-alerta"><?php echo $msg ?></div>
                     </div>
