42e6f2da11e0a8550dd7137e07c445e73a1e72d2
AfonsoFernando051 <afonsofernando051@gmail.com>
2023-11-19 21:18:53 -0300

    backend de msg funcionando

--- questao-3/ClassSendMessage.php
+++ questao-3/ClassSendMessage.php
@@ -1,43 +1,44 @@
 <?php
+require 'vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 // Carregue a biblioteca PHPMailer
-require './PHPMailer/src/Exception.php';
-require './PHPMailer/src/PHPMailer.php';
-require './PHPMailer/src/SMTP.php';
+
 $config = include './config.php';
 
-print_r($config);
 $email = $config['email'];
 $senha = $config['senha'];
+$name = $config['name'];
 
 if(isset($_POST["btnSend"])){
-    // Configurações do servidor de email
     $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 8000;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->SMTPAuth = true;
-    $mail->Username = $email;
-    $mail->Password = $senha;
-    $mail->AuthType = 'PLAIN';
-    $mail->SMTPDebug = 2; // Habilita o modo de depuração
-
-    // Configurações do email
-    $mail->isHTML(true);
-    $mail->setFrom($email);
-    $mail->addAddress($_POST['email'], $_POST['nome']);
-    $mail->Body = $_POST['mensagem'];
-    
-    // Adicione os campos do formulário ao corpo do email
-    $mail->Body .= "\n\nNome: " . $_POST['nome'];
-    $mail->Body .= "\nTelefone: " . $_POST['telefone'];
-    $mail->Body .= "\nEmail: " . $_POST['email'];
-    $mail->Body .= "\nMensagem: " . $_POST['mensagem'];
-    
     try {
+        // Configurações do servidor de email
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $email;
+        $mail->Password = $senha;
+        $mail->SMTPSecure = 'tsl';
+        $mail->Port = 587;
+        $mail->AuthType = 'PLAIN';
+        $mail->CharSet = 'UTF-8';
+
+        // Configurações do email
+        $mail->setFrom($email, $name);
+        $mail->addAddress($_POST['email'], $_POST['nome']);
+        $mail->addReplyTo($_POST['email'], $_POST['nome']);
+        $mail->Body = $_POST['mensagem'];
+        
+        // Adicione os campos do formulário ao corpo do email
+        $mail->Body .= "\n\nNome: " . $_POST['nome'];
+        $mail->Body .= "\nTelefone: " . $_POST['telefone'];
+        $mail->Body .= "\nEmail: " . $_POST['email'];
+        $mail->Body .= "\nMensagem: " . $_POST['mensagem'];
+    
         // Tenta enviar o email
         $mail->send();
         echo "
